08d81a4f181bac13bc0f2aff3f7e5cb7109818e5
Hanting Wong <hanwong@amazon.com>
2022-06-06 17:15:43 -0400

    comment

--- cli/src/pcluster/cli/entrypoint.py
+++ cli/src/pcluster/cli/entrypoint.py
@@ -11,7 +11,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
 # implied. See the License for the specific language governing permissions and
 # limitations under the License.
-# random
+# random thing
 
 import inspect
 import json
